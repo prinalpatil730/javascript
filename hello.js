886d2c3cd1b3e6fa1111010e31ce5b4d705cf908
Prinal Pramod Patil <prinalpatil730@gmail.com>
2026-01-02 23:32:45 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*
+  Day 1: JavaScript Basics
+  Topic: Hello World & console.log
+*/
+
+// Print a simple message
+console.log("Hello, JavaScript! 🚀");
+
+// Print learning goal
+console.log("Day 1 completed: Started my JavaScript journey");
+
+// Example of printing multiple values
+console.log("Name:", "Prinal");
+console.log("Status:", "JavaScript Beginner");
