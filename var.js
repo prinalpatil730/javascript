19ed6c03290546bd87e1cedb85eb1412b9348040
Prinal Pramod Patil <prinalpatil730@gmail.com>
2026-01-03 20:19:41 +0530

    Create var.js

---
+++ var.js
@@ -0,0 +1,9 @@
+// variables.js
+
+let name = "Prinal";
+let age = 22;
+let isLearningJS = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Learning JavaScript:", isLearningJS);
